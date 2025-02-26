a77fd356a85fd7d65b1a8dd48bd4041977262161
draysams <rsamdavies@gmail.com>
2025-02-26 08:40:50 +0000

    feat(empty-string): add support for empty strings

--- hello.go
+++ hello.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 
+const helloWorldPrefix = "Hello, "
 func Hello(name string) string {
-	return "Hello, "+ name
+	if name == "" {
+		name = "world"
+	}
+	return helloWorldPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello(""))
 }
