d17362e220435e54a7fb26afdb1dd684f50cb3b5
draysams <rsamdavies@gmail.com>
2025-03-01 17:56:47 +0000

    feat(search): implement search on dictionary

---
+++ maps/dictionary_test.go
@@ -0,0 +1,19 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is a test"
+
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got string, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q, given %q", got, want, "test")
+	}
+}
