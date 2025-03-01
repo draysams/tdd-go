b293f593cbb2ee38e623aa506c982c1f88d53d9d
draysams <rsamdavies@gmail.com>
2025-03-01 11:27:19 +0000

    refactor(test-wallet): add assertBalance helper

--- pointers_errors/wallet_test.go
+++ pointers_errors/wallet_test.go
@@ -1,38 +1,30 @@
 package pointers_errors
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestBank(t *testing.T) {
+func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, expected Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		want := expected
+
+		if got != want {
+			t.Errorf("got %s but expected %s", got, want)
+		}
+	}
 
 	t.Run("deposit amount", func(t *testing.T) {
 		wallet := Wallet{}
-
 		wallet.Deposit(10)
-
-		got := wallet.Balance()
-
-		fmt.Printf("in test address of my balance %p \n", &wallet.balance)
-
-		balance := Bitcoin(10)
-
-		if got != balance {
-			t.Errorf("got %s but expected %s", got, balance)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw amount from wallet", func(t *testing.T) {
 		wallet := Wallet{balance: 20}
-
 		wallet.Withdraw(10)
-		got := wallet.Balance()
-		expected := Bitcoin(10)
-
-		if got != expected {
-			t.Errorf("got %s but expected %s", got, expected)
-		}
+		assertBalance(t, wallet, Bitcoin(10))
 
 	})
 }
