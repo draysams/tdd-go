2b1eb26bbb54a6f7baf67c7f1c7c5bc587f52f19
draysams <rsamdavies@gmail.com>
2025-03-01 08:49:48 +0000

    feat(tailsSum): implement sum all tails

--- array_slice/sum_test.go
+++ array_slice/sum_test.go
@@ -21,8 +21,33 @@ func TestSum(t *testing.T) {
 func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{3, 4})
 	expected := []int{3, 7}
+	given := [][]int{{1, 2}, {3, 4}}
 
+	checkSums(t, got, expected, given)
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("sum a slice's tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{2, 3, 4})
+		expected := []int{5, 7}
+		given := [][]int{{1, 2, 3}, {2, 3, 4}}
+
+		checkSums(t, got, expected, given)
+
+	})
+
+	t.Run("sum an empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+		given := [][]int{{}, {3, 4, 5}}
+
+		checkSums(t, got, expected, given)
+	})
+}
+
+func checkSums(t testing.TB, got []int, expected []int, given [][]int) {
+	t.Helper()
 	if !slices.Equal(got, expected) {
-		t.Errorf("expected %v but got %v given %v", expected, got, [][]int{{1, 2}, []int{3, 4}})
+		t.Errorf("expected %v gpt %v given %v", expected, got, given)
 	}
 }
