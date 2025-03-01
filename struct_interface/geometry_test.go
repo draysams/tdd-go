33626e205c8ef57fa1f3e6417f59ff5d5775abd4
draysams <rsamdavies@gmail.com>
2025-03-01 10:09:16 +0000

    refactor(geometry): support area calculation for multiple shapes

--- struct_interface/geometry_test.go
+++ struct_interface/geometry_test.go
@@ -13,11 +13,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12, 6}
-	got := Area(rectangle)
-	expected := float64(72)
-
-	if got != expected {
-		t.Errorf("expected %.2f but got %.2f", expected, got)
+	checkArea := func(t testing.TB, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != expected {
+			t.Errorf("expected %g but got %g", expected, got)
+		}
 	}
+
+	t.Run("area of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		expected := float64(72)
+
+		checkArea(t, rectangle, expected)
+	})
+
+	t.Run("area of a circle", func(t *testing.T) {
+		circle := Circle{10}
+		expected := 314.1592653589793
+
+		checkArea(t, circle, expected)
+	})
 }
